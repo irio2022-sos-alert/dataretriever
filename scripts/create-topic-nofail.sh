e7b50083a6ed68b3dfaa486a0991a9d43b87663b
Kamil <kamil.jonak18@gmail.com>
2023-01-27 00:47:10 +0100

    remove schema

--- scripts/create-topic-nofail.sh
+++ scripts/create-topic-nofail.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 readonly topic="$1"
-readonly schema="$2"
-gcloud pubsub topics create $topic --message-encoding=json --schema=$schema || true
+
+gcloud pubsub topics create $topic --message-encoding=json || true
