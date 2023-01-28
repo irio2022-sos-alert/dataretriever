22b69f829cbfb818b1a08da883aa9ea9fcc1d3fb
Kamil <kamil.jonak18@gmail.com>
2023-01-28 20:31:24 +0100

    removed redundant models

--- whistleblower/models.py
+++ whistleblower/models.py
@@ -14,23 +14,5 @@ class Service(SQLModel, table=True):
 
 class Responses(SQLModel, table=True):
     __tablename__ = "responses"
-    service_id: int = Field(foreign_key="services.id", primary_key=True)
+    service_id: int = Field(foreign_key="service.id", primary_key=True)
     timestamp: int
-
-
-class Admins(SQLModel, table=True):
-    __tablename__ = "admins"
-    id: int = Field(primary_key=True)
-    email: str
-
-
-class Ownership(SQLModel, table=True):
-    __tablename__ = "ownership"
-    service_id: int = Field(foreign_key="services.id", primary_key=True)
-    admin_id: int = Field(foreign_key="admins.id", primary_key=True)
-    first_contact: bool
-
-
-class Alerts(SQLModel, table=True):
-    __tablename__ = "alerts"
-    service_id: int = Field(foreign_key="services.id", primary_key=True)
