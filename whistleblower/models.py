e6dc741facdbd7a7bc6efec4ab5e53d64d7e030d
Kamil <kamil.jonak18@gmail.com>
2023-01-28 16:26:10 +0100

    test copy db

--- whistleblower/models.py
+++ whistleblower/models.py
@@ -1,3 +1,18 @@
+# from sqlmodel import Field, SQLModel
+
+
+# class Services(SQLModel, table=True):
+#     __tablename__ = "services"
+#     id: int = Field(primary_key=True)
+#     name: str
+
+
+# class Responses(SQLModel, table=True):
+#     __tablename__ = "responses"
+#     service_id: int = Field(foreign_key="services.id", primary_key=True)
+#     time: float
+#     positive: bool
+
 from sqlmodel import Field, SQLModel
 
 
@@ -7,8 +22,19 @@ class Services(SQLModel, table=True):
     name: str
 
 
-class Responses(SQLModel, table=True):
-    __tablename__ = "responses"
+class Admins(SQLModel, table=True):
+    __tablename__ = "admins"
+    id: int = Field(primary_key=True)
+    email: str
+
+
+class Ownership(SQLModel, table=True):
+    __tablename__ = "ownership"
     service_id: int = Field(foreign_key="services.id", primary_key=True)
-    time: float
-    positive: bool
+    admin_id: int = Field(foreign_key="admins.id", primary_key=True)
+    first_contact: bool
+
+
+class Alerts(SQLModel, table=True):
+    __tablename__ = "alerts"
+    service_id: int = Field(foreign_key="services.id", primary_key=True)
