4b690106d95f6ea961955136ce66f56a68bb5825
Kamil <kamil.jonak18@gmail.com>
2023-01-28 14:36:27 +0100

    removed tcp connection

--- whistleblower/db.py
+++ whistleblower/db.py
@@ -5,43 +5,43 @@ import sqlalchemy
 from sqlmodel import Session, SQLModel
 
 
-def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
-    """Initializes a TCP connection pool for a Cloud SQL instance of Postgres.
-    Useful for testing and, or running in a local docker container with whitelisted IP."""
-    db_host = os.getenv("INSTANCE_HOST", "[::]")  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
-    db_user = os.getenv("DB_USER")  # e.g. 'my-db-user'
-    db_pass = os.getenv("DB_PASS")  # e.g. 'my-db-password'
-    db_name = os.getenv("DB_NAME")  # e.g. 'my-database'
-    db_port = os.getenv("DB_PORT")  # e.g. 5432
-    connect_args = {}
+# def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
+#     """Initializes a TCP connection pool for a Cloud SQL instance of Postgres.
+#     Useful for testing and, or running in a local docker container with whitelisted IP."""
+#     db_host = os.getenv("INSTANCE_HOST")  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
+#     db_user = os.getenv("DB_USER")  # e.g. 'my-db-user'
+#     db_pass = os.getenv("DB_PASS")  # e.g. 'my-db-password'
+#     db_name = os.getenv("DB_NAME")  # e.g. 'my-database'
+#     db_port = os.getenv("DB_PORT")  # e.g. 5432
+#     connect_args = {}
 
-    if os.environ.get("DB_ROOT_CERT"):
-        db_root_cert = os.environ["DB_ROOT_CERT"]  # e.g. '/path/to/my/server-ca.pem'
-        db_cert = os.environ["DB_CERT"]  # e.g. '/path/to/my/client-cert.pem'
-        db_key = os.environ["DB_KEY"]  # e.g. '/path/to/my/client-key.pem'
+#     if os.environ.get("DB_ROOT_CERT"):
+#         db_root_cert = os.environ["DB_ROOT_CERT"]  # e.g. '/path/to/my/server-ca.pem'
+#         db_cert = os.environ["DB_CERT"]  # e.g. '/path/to/my/client-cert.pem'
+#         db_key = os.environ["DB_KEY"]  # e.g. '/path/to/my/client-key.pem'
 
-        ssl_context = ssl.SSLContext()
-        ssl_context.verify_mode = ssl.CERT_REQUIRED
-        ssl_context.load_verify_locations(db_root_cert)
-        ssl_context.load_cert_chain(db_cert, db_key)
-        connect_args["ssl_context"] = ssl_context
+#         ssl_context = ssl.SSLContext()
+#         ssl_context.verify_mode = ssl.CERT_REQUIRED
+#         ssl_context.load_verify_locations(db_root_cert)
+#         ssl_context.load_cert_chain(db_cert, db_key)
+#         connect_args["ssl_context"] = ssl_context
 
-    pool = sqlalchemy.create_engine(
-        sqlalchemy.engine.url.URL.create(
-            drivername="postgresql+pg8000",
-            username=db_user,
-            password=db_pass,
-            host=db_host,
-            port=db_port,
-            database=db_name,
-        ),
-        connect_args=connect_args,
-        pool_size=5,
-        max_overflow=2,
-        pool_timeout=30,  # 30 seconds
-        pool_recycle=1800,  # 30 minutes
-    )
-    return pool
+#     pool = sqlalchemy.create_engine(
+#         sqlalchemy.engine.url.URL.create(
+#             drivername="postgresql+pg8000",
+#             username=db_user,
+#             password=db_pass,
+#             host=db_host,
+#             port=db_port,
+#             database=db_name,
+#         ),
+#         connect_args=connect_args,
+#         pool_size=5,
+#         max_overflow=2,
+#         pool_timeout=30,  # 30 seconds
+#         pool_recycle=1800,  # 30 minutes
+#     )
+#     return pool
 
 
 def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
@@ -72,8 +72,8 @@ def connect_unix_socket() -> sqlalchemy.engine.base.Engine:
 
 def init_connection_pool() -> sqlalchemy.engine.base.Engine:
     # use a TCP socket when INSTANCE_HOST (e.g. 127.0.0.1) is defined
-    if os.environ.get("INSTANCE_HOST"):
-        return connect_tcp_socket()
+    # if os.environ.get("INSTANCE_HOST"):
+    #     return connect_tcp_socket()
 
     # use a Unix socket when INSTANCE_UNIX_SOCKET (e.g. /cloudsql/project:region:instance) is defined
     if os.environ.get("INSTANCE_UNIX_SOCKET"):
